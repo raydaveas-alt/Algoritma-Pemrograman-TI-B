854fabd94e071ef9d68960b0e6e769bdd3815bae
raydaveas-alt <redeabcdefg@gmail.com>
2026-02-03 21:18:04 +0700

    i

--- Tugas praktikum/soal 13.py
+++
@@ -1,9 +0,0 @@
-# Soal 1.3
-print("Soal 1.3")
-nilai = 75
-print(f"Nilai: {nilai}")
-if nilai >= 60:
-    print("Lulus.")
-else:
-    print("Tidak lulus.")
-print()
--- Tugas praktikum/soal 14.py
+++
@@ -1,9 +0,0 @@
-# Soal 1.4
-print("Soal 1.4")
-total = 0
-
-for i in range(1, 11):
-    total += i
-
-print("Jumlah:", total)
-print()
